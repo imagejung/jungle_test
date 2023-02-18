031612c9f88c283d016f54a1291d426f1b06feaa
YoungSang Jung <mikey5843@naver.com>
2023-02-18 16:36:40 +0900

    기능구현 완료

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스�
 
 app = Flask(__name__)
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.ecijcpt.mongodb.net/?retryWrites=true&w=majority') #cloud
+# client = MongoClient('localhost', 27017)
 # mongoDB는 27017 포트로 돌아갑니다.
+client = MongoClient('mongodb+srv://test:sparta@cluster0.ecijcpt.mongodb.net/?retryWrites=true&w=majority') #cloud
 db = client.dbjungle  # 'dbjungle'라는 이름의 db를 만들거나 사용합니다.
 
 
