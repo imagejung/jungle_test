c61d89df6e589847d1ad7e8d649ae10778a790d5
YoungSang Jung <mikey5843@naver.com>
2023-02-18 12:04:59 +0900

    listing까지

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, jsonify, request
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+
+app = Flask(__name__)
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.ecijcpt.mongodb.net/?retryWrites=true&w=majority') #cloud
+# mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbjungle  # 'dbjungle'라는 이름의 db를 만들거나 사용합니다.
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/memo', methods=['POST'])
+def post_article():
+    # 1. 클라이언트로부터 데이터를 받기
+    memo_receive = request.form['memo_give']  # 클라이언트로부터 url을 받는 부분
+    comment_receive = request.form['comment_give']  # 클라이언트로부터 comment를 받는 부분
+
+    memos = {'memo': memo_receive, 'comment': comment_receive}
+
+    # 3. mongoDB에 데이터를 넣기
+    db.memos.insert_one(memos)
+
+    return jsonify({'result': 'success'})
+
+
+
+@app.route('/memo', methods=['GET'])
+def read_articles():
+    # 1. mongoDB에서 _id 값을 제외한 모든 데이터 조회해오기 (Read)
+    result = list(db.memos.find({}, {'_id': 0}))
+    # 2. articles라는 키 값으로 article 정보 보내주기
+    return jsonify({'result': 'success', 'memos': result})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
